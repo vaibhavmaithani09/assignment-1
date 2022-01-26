dc00f9ea58ab57ddc5bb7e278b20766188613855
vaibhavmaithani09 <97669712+vaibhavmaithani09@users.noreply.github.com>
2022-01-26 19:54:01 +0530

    Add files via upload

---
+++ assignment 2 output.py
@@ -0,0 +1,41 @@
+***question1***
+length of the input string is 36
+.egaugnal evitisnes esac a si nohtyP
+a case sensitive
+Python is object oriented language.
+index of a in input string is  10
+Python is a case sensitive language.
+
+***question2***
+hey, Vaibhav Maithani here!
+My sid is 21103048 
+I am from Computer Science and Engineering department and my cgpa is 9.8
+
+***question3***
+a&b is 8
+a|b is 58
+a^b is  50
+left shifting both a and b with two bits gives a= 224  and b= 40
+right shifting a with 2 bits and b with 4 bits gives a= 14 and b= 0
+
+***question4***
+enter first number
+20
+enter second number
+30
+enter third number
+35
+the greatest of three nos is 35
+
+***question5***
+enter a string my name is vaibhav
+yes
+
+***question6***
+enter length of first side
+8
+enter length of second side
+6
+enter length of third side
+10
+yes, triangle can be formed.
