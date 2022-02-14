8c8c0183c78340c684b4711107153195c1886e41
vaibhavmaithani09 <97669712+vaibhavmaithani09@users.noreply.github.com>
2022-02-14 17:43:50 +0530

    Add files via upload

---
+++ assignment3output.py
@@ -0,0 +1,123 @@
+***question1***
+
+Enter a string
+it was the best of times,it was the worst of times,it was the age of wisdom,it was the age of foolishness,it was the epoch of belief
+The frequency of it is 1
+The frequency of was is 5
+The frequency of the is 5
+The frequency of best is 1
+The frequency of of is 5
+The frequency of times,it is 2
+The frequency of worst is 1
+The frequency of age is 2
+The frequency of wisdom,it is 1
+The frequency of foolishness,it is 1
+The frequency of epoch is 1
+The frequency of belief is 1
+
+
+
+***question2***
+
+enter the date (1-31)
+12
+enter the month (1-12)
+10
+enter the year (1800-2025)
+2022
+the input date is 12 / 10 / 2022
+the next date is 13 / 10 / 2022
+
+
+***question3***
+
+Type 'y' to enter number / Type 'n' to cancel
+y
+Enter the number
+5
+Type 'y' to enter number / Type 'n' to cancel
+y
+Enter the number
+6
+Type 'y' to enter number / Type 'n' to cancel
+y
+Enter the number
+7
+Type 'y' to enter number / Type 'n' to cancel
+n
+Numbers you entered are : [5, 6, 7]
+Given number, square of that number : [(5, 25), (6, 36), (7, 49)]
+
+
+
+
+
+
+***question4***
+
+Please enter the grade (4-10)
+9
+Your Grade is ‘A’ and Excellent Performance.
+
+
+
+***question5***
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+***question6***
+
+Type 'y' to enter Name and SID / Type 'n' to cancel
+y
+Enter the SID
+21103048
+Enter the Name
+Vaibhav
+Type 'y' to enter Name and SID / Type 'n' to cancel
+y
+Enter the SID
+21103099
+Enter the Name
+rahul
+Type 'y' to enter Name and SID / Type 'n' to cancel
+y
+Enter the SID
+21103096
+Enter the Name
+rohit
+Type 'y' to enter Name and SID / Type 'n' to cancel
+n
+{'21103048': 'Vaibhav', '21103099': 'rahul', '21103096': 'rohit'}
+Dictionary after sorting it by key is : {'21103048': 'Vaibhav', '21103096': 'rohit', '21103099': 'rahul'}
+Dictionary after sorting it by value is : {'21103048': 'Vaibhav', '21103099': 'rahul', '21103096': 'rohit'}
+Enter the student's SID which you want to find
+21103048
+Name of the student whose SID is 21103048 is Vaibhav
+
+***question 7***
+
+Enter the number of terms you want to print in the fibonacci sequence
+15
+Fibonacci sequence:
+[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
+The average of fibonacci sequence upto 15  terms is 65.73333333333333
+
+***question8***
+Set of all elements that are in Set1 and Set2 but not in both is {1, 3, 5, 6, 8}
+Set of all elements that are in only one of the three sets Set1, Set2 and Set3 is {3, 6, 8, 9, 13, 17}
+Set of elements that are exactly two of the sets Set1, Set2 and Set3 is {1, 2, 4, 5}
+Set of all integers in the range 1 to 10 that are not in Set1 is {6, 7, 8, 9, 10}
+set of all integers in the range 1 to 10 that are not in Set1, Set2 and Set3 is {10, 7}
+
+
+
+
+
+
+
+
