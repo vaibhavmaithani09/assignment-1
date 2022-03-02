4421fbff648080c1ece5f82f05080a7ce08650dc
vaibhavmaithani09 <97669712+vaibhavmaithani09@users.noreply.github.com>
2022-03-02 11:53:58 +0530

    Add files via upload

---
+++ assignment4output.py
@@ -0,0 +1,84 @@
+*****question1*****
+
+Move disk 1 from source A to C
+Move disk 2 from source A to B
+Move disk 1 from source C to B
+Move disk 3 from source A to C
+Move disk 1 from source B to A
+Move disk 2 from source B to C
+Move disk 1 from source A to C
+Move disk 4 from source A to B
+Move disk 1 from source C to B
+Move disk 2 from source C to A
+Move disk 1 from source B to A
+Move disk 3 from source C to B
+Move disk 1 from source A to C
+Move disk 2 from source A to B
+Move disk 1 from source C to B
+
+*****question2*****
+Enter number of rows you want to print in Pascal's Triangle using recursive approach
+6
+The Pascal Triangle by using recursive method is :
+          1   
+        1   1   
+      1   2   1   
+    1   3   3   1   
+  1   4   6   4   1   
+1   5   10  10  5   1   
+Enter number of rows you want to print in Pascal's Triangle using iterative approach
+
+
+*****question3******
+Enter the dividend :
+60
+Enter the divisor :
+15
+The quotient is 4 and the remainder is 0
+The statement that divmod function is callable is False .
+The statement that quotient is callable is False and the statement that remainder is callable is False .
+The statement that dividend is callable is False and the statement that divisor is callable is False .
+The values we have are : {4: 'Quotient', 0: 'Remainder'}
+The quotient or remainder or both are zero.
+Adding values (4, 5, 6) to the result and filtering out the values which are greater than 4.
+[5, 6]
+Converting it into set datatype
+{5, 6}
+Making the set immutable
+frozenset({5, 6})
+The maximum value from set is 6
+Its hash value is 6221592716431983538
+
+
+*****question4******
+Name: Aaersh Kumar , Roll number: 21103037
+Name: Chirag , Roll number: 21110332
+Name: Manoj , Roll number: 21102044
+Name: Hammad , Roll number: 21712336
+After deletion:
+Name: Aaersh Kumar , Roll number: 21103037
+Name: Manoj , Roll number: 21102044
+Name: Hammad , Roll number: 21712336
+
+*****question5******
+There are 3 employees
+Details of all employee:
+Name: Mehak , Salary: 40000
+Name: Ashok , Salary: 50000
+Name: Viren , Salary: 60000
+After updation:
+There are 2 employees
+Details of all employee:
+Name: Mehak , Salary: 70000
+Name: Ashok , Salary: 50000
+
+*****question6*******
+Question number 6
+Enter the first word: sweat
+
+Enter a new meaningful word with the exact same letters : waste
+
+Does the word makes sense?(y or n)
+y
+The friends pass the friendship test
+
